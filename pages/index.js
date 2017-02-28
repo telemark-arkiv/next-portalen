e455d79ffd9e5f6c402d35b2c8edc6bece8ebf1b
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-02-28 16:19:46 +0100

    Standard fixes

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@ import Link from 'next/link'
 export default () => (
   <div>
     <h1>Hallo, venn</h1>
-<ul>
-<li><Link href='/b' as='/a'><a>a</a></Link></li>
-  <li><Link href='/a' as='/b'><a>b</a></Link></li>
-  </ul>
+    <ul>
+      <li><Link href='/b' as='/a'><a>a</a></Link></li>
+      <li><Link href='/a' as='/b'><a>b</a></Link></li>
+    </ul>
   </div>
 )
