68c2679f62e5b032cbd7519ee6a59517ed296cc1
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-03-01 15:41:16 +0100

    Swicthes to pure next

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 'use strict'
 
 import React from 'react'
+import Head from '../components/head'
 
-export default () => (
-  <div>
-    <h1>Hallo, venn</h1>
-  </div>
-)
+export default class Index extends React.Component {
+
+  static async getInitialProps (ctx) {
+    const session = {
+      user: {
+        displayName: 'Tøffen'
+      }
+    }
+    return {session: session}
+  }
+
+  render () {
+    return (
+      <html>
+        <Head />
+        <div>
+          <h1>Hallo, {this.props.session.user.displayName}</h1>
+        </div>
+      </html>
+    )
+  }
+}
